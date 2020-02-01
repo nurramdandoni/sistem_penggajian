d1a37644ee227ce07f45304f808a28176c10e4a0
PPSI <nurramdandoni@gmail.com>
2020-02-01 08:56:17 +0700

    fix layout data Karyawan

--- application/views/data_karyawan.php
+++ application/views/data_karyawan.php
@@ -20,12 +20,12 @@ div.dataTables_paginate{
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Data Master</h1>
+            <h1>Data Karyawan</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">DataTables</li>
+              <li class="breadcrumb-item"><a href="#">Data Master</a></li>
+              <li class="breadcrumb-item active">Data Karyawan</li>
             </ol>
           </div>
         </div>
@@ -38,40 +38,100 @@ div.dataTables_paginate{
         <div class="col-12">
           <div class="card">
             <div class="card-header">
-              <h1 class="card-title">Data Karyawan</h1>
+              <!-- <h1 class="card-title">Data Karyawan</h1> -->
+              <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button">+ Tambah Data Karyawan</a>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
               <table id="example1" class="table table-bordered table-hover">
                 <thead>
                 <tr>
-                  <th>Nama Karyawan</th>
-                  <th>Divisi</th>
-                  <th>Jabatan</th>
-                  <th>Tanggal Recruitment</th>
-                  <th>Aksi</th>
+                  <th style="text-align:center;">Nama Karyawan</th>
+                  <th style="text-align:center;">Divisi</th>
+                  <th style="text-align:center;">Jabatan</th>
+                  <th style="text-align:center;">Tanggal Recruitment</th>
+                  <th style="text-align:center;">Aksi</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
-                  <td>Trident</td>
-                  <td>Internet
-                    Explorer 4.0
+                  <td>Doni Nurramdan, A. Md.</td>
+                  <td>Pusat Pengembangan Sistem Informasi (PPSI)</td>
+                  <td style="text-align:center;">Staff/ Programmer WebApps (FullStack)</td>
+                  <td style="text-align:center;">23 Mei 2018</td>
+                  <td style="text-align:center;">
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-eye"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-edit"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-trash-b"></i></a>
                   </td>
-                  <td>Win 95+</td>
-                  <td> 4</td>
-                  <td>X</td>
+                </tr>
+                <tr>
+                  <td>Muhammad Faqih, S. Kom.</td>
+                  <td>Pusat Pengembangan Sistem Informasi (PPSI)</td>
+                  <td style="text-align:center;">Staff/ Programmer Mobile (FullStack)</td>
+                  <td style="text-align:center;">Agustus 2018</td>
+                  <td style="text-align:center;">
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-eye"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-edit"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-trash-b"></i></a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Cintiya Dewiani Putri, S. Kom.</td>
+                  <td>Pusat Pengembangan Sistem Informasi (PPSI)</td>
+                  <td style="text-align:center;">Staff/ Programmer Mobile (FullStack)</td>
+                  <td style="text-align:center;">15 Januari 2020</td>
+                  <td style="text-align:center;">
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-eye"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-edit"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-trash-b"></i></a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Titi Widaretna, S. T.</td>
+                  <td>Pusat Pengembangan Sistem Informasi (PPSI)</td>
+                  <td style="text-align:center;">Staff/ Kepala PPSI</td>
+                  <td style="text-align:center;">-</td>
+                  <td style="text-align:center;">
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-eye"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-edit"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-trash-b"></i></a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Gia Yuliana, S. Kom.</td>
+                  <td>Pusat Pengembangan Sistem Informasi (PPSI)</td>
+                  <td style="text-align:center;">Staff/ Programmer/ System Documentation</td>
+                  <td style="text-align:center;">-</td>
+                  <td style="text-align:center;">
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-eye"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-edit"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-trash-b"></i></a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Andri Nugroho, S. Kom.</td>
+                  <td>Pusat Pengembangan Sistem Informasi (PPSI)</td>
+                  <td style="text-align:center;">Staff/ Programmer/ Front End</td>
+                  <td style="text-align:center;">-</td>
+                  <td style="text-align:center;">
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-eye"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-edit"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-trash-b"></i></a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Syifa Nur Fauziah, S. Kom.</td>
+                  <td>Biro Administrasi Akademik dan Kemahasiswaan (BAAK)</td>
+                  <td style="text-align:center;">Staff/ Kepala BAAK</td>
+                  <td style="text-align:center;">-</td>
+                  <td style="text-align:center;">
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-eye"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-edit"></i></a>
+                    <a type="button" class="btn btn-success" href="<?php echo base_url()?>admin" role="button"><i class="ion ion-trash-b"></i></a>
+                  </td>
                 </tr>
                 </tbody>
-                <tfoot>
-                <tr>
-                    <th>Nama Karyawan</th>
-                    <th>Divisi</th>
-                    <th>Jabatan</th>
-                    <th>Tanggal Recruitment</th>
-                    <th>Aksi</th>
-                </tr>
-                </tfoot>
               </table>
             </div>
             <!-- /.card-body -->
